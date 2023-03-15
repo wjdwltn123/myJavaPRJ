8d7c3c67775dbab07665528e755e64aa73a4c96a
wjdwltn123 <jungjisu0809@gmail.com>
2023-03-15 09:33:42 +0900

    소스 수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello!! My First Java Coding!!");
         System.out.println("have a great day!");
+
+        System.out.println("앞으로 열심히 하겠습니다");
+
     }
 }
