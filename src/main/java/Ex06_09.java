dca651ad44ffd6978514dc496c898ca2808916e9
wjdwltn123 <jungjisu0809@gmail.com>
2023-03-31 17:07:02 +0900

    연습용

---
+++ src/main/java/Ex06_09.java
@@ -0,0 +1,12 @@
+public class Ex06_09 {
+    public static void main(String[] args) {
+        int hap= 0;
+                int i;
+
+        for ( i = 501; i< 1000; i+=2){
+            hap +=i;
+
+            System.out.printf("500에서 1000까지의 홀수의 합 : \n", hap);
+        }
+    }
+}
