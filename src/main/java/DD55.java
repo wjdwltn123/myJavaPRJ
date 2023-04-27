5ef5851751adad42694c9cf28389dfad76594c3b
wjdwltn123 <jungjisu0809@gmail.com>
2023-04-27 09:36:18 +0900

    연습

---
+++ src/main/java/DD55.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DD55 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        if (a % 3 == 0 && a% 5 == 0) {
+            System.out.println("3의 배수와 5의 배수를 둘다 만족합니다.");
+        } else if (a % 3 == 0) {
+            System.out.println("3의 배수입니다.");
+        } else if (a % 5 == 0) {
+            System.out.println("5의 배수입니다.");
+        } else {
+            System.out.println("해당 조건을 만족하지 않습니다.");
+        }
+    }
+}
