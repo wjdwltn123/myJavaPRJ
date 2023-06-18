e0cc24a63b0fbe45a71cefe9616d3aafdb9fe93f
wjdwltn123 <jungjisu0809@gmail.com>
2023-06-19 08:44:59 +0900

    100개의 정수를 저장할 수 있는 배열을 선언하고 정수를 차례로 입력받다가 0이 입력되면 0을 제외하고 그때까지 입력된 정수를 가장 나중에 입력된 정수부터 차례대로 출력

---
+++ src/main/java/Practice/Prac19.java
@@ -0,0 +1,20 @@
+package Practice;
+import java.util.Scanner;
+
+public class Prac19 {
+    public static void main(String[] args) {
+        int[] intArr = new int[100];
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<intArr.length; i++) {
+            System.out.print("정수 입력 : ");
+            intArr[i] = sc.nextInt();
+            if(intArr[i] == 0) break;
+
+            count++;
+        }
+        for(int i = 0; i <= count; i++) {
+            System.out.print(intArr[count-i] + " ");
+        }
+    }
+}
